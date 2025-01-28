0bd0bbfe2b597bfd41034b3cf585a525a655c810
Alexandre <lassaignealexandre@gmail.com>
2025-01-28 10:19:51 +0100

    MorningNews

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch('https://morning-news-backend-nine.vercel.app/articles')
+    fetch('http://localhost:3000/articles')
       .then(response => response.json())
       .then(data => {
         setTopArticle(data.articles[0]);
