a183143e772de64fdc973471bdb80fbe8d45e153
Alexandre <lassaignealexandre@gmail.com>
2025-02-06 16:07:30 +0100

    commit

--- components/Article.js
+++ components/Article.js
@@ -14,7 +14,7 @@ function Article(props) {
 		if (!user.token) {
 			return;
 		}
-		fetch(`https://morning-news-backend-murex.vercel.app/users/canBookmark/${user.token}`)
+		fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
 			.then(response => response.json())
 			.then(data => {
 				if (data.result && data.canBookmark) {
--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch('https://morning-news-backend-murex.vercel.app/articles')
+    fetch('http://localhost:3000/articles')
       .then(response => response.json())
       .then(data => {
         setTopArticle(data.articles[0]);
